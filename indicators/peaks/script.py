c5cc9b3608afce46ff96a81d4a5fdc119e135d7b
martin <martinbarut@hotmail.com>
2022-08-24 15:52:19 +0200

    init commit - added peaks

---
+++ indicators/peaks/script.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+def create_data_config(params):
+    return {
+        "indicators": [
+            {
+                "type": params["indicator_type"],
+                "name": params["indicator_name"],
+                "params": params["indicator_params"]
+            }
+        ]
+    }
+
+
+def calculate(data, params):
+    """
+    todo: explain the data format here:
+    """
+    output = []
+    indicator_name = params["indicator_name"]
+    series_records = data["indicators"][indicator_name]  # [{timestamp: ..., value: ...}]
+    series_arr = np.array([r["value"] for r in series_records])
+    for t in range(len(series_arr)):
+        if is_peak(t, series_arr, **params):
+            output.append({
+                "timestamp": series_records[t]["timestamp"],
+                "value": series_records[t]["value"]
+            })
+        else:
+            output.append({
+                "timestamp": series_records[t]["timestamp"],
+                "value": None
+            })
+    return output
+
+
+def is_peak(t, series_arr, sdev_length, sdev_boundary, peak_lookback, peak_lookahead, **kwargs):
+    window_from = t - peak_lookback
+    window_until = t + peak_lookahead
+    sdev_until = t + peak_lookahead
+    sdev_from = sdev_until - sdev_length
+
+    if window_from < 0 or sdev_from < 0:
+        return False
+    # note: series like rsi have None values in the beginning
+    if series_arr[window_from] is None or series_arr[sdev_from] is None:
+        return False
+    if window_until > len(series_arr) - 1:
+        return False
+
+    window = series_arr[window_from: window_until + 1]
+    max_val = np.max(window)
+    mid_val = series_arr[t]
+    if mid_val != max_val:
+        return False
+
+    left_val = window[0]
+    right_val = window[-1]
+    # note: for more precision (less recall) let delta = min(...)
+    delta = max(max_val - left_val, max_val - right_val)
+    sdev = np.std(series_arr[sdev_from:sdev_until + 1])
+
+    if delta > sdev_boundary * sdev:
+        return True
+    else:
+        return False
+
---
+++ patterns/peaks/script.py
@@ -0,0 +1,41 @@
+import math
+
+
+def create_data_config(params):
+    return {
+        "indicators": [
+            {
+                "type": "price_close",
+                "name": "price_close",
+                "params": {
+                    "interval": params["interval"]
+                }
+            },
+            {
+                "type": "peaks",
+                "name": "price_peaks",
+                "params": {
+                    "indicator_type": "price_close",
+                    "indicator_name": "price_close",
+                    "indicator_params": {
+                        "interval": params["interval"]
+                    },
+                    "peak_lookback": params["peak_lookback"],
+                    "peak_lookahead": params["peak_lookahead"],
+                    "sdev_boundary": params["sdev_boundary"],
+                    "sdev_length": params["sdev_length"]
+                }
+            }
+        ]
+    }
+
+
+def signal(t, data, params):
+    if math.isnan(data[t]["price_peaks"]):
+        return False, None
+    else:
+        meta = {
+            "signal_period": t,
+            "signal_timestamp": int(data[t]["timestamp"])
+        }
+        return True, meta
