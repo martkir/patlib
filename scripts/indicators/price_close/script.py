08c594f892571df62983257d9a12d2b66221e22c
martin <martinbarut@hotmail.com>
2022-09-04 16:46:25 +0200

    removed unused code

--- scripts/indicators/price_close/script.py
+++ scripts/indicators/price_close/script.py
@@ -15,18 +15,6 @@ def create_data_config(params):
 
 
 def calculate(data, params):
-    """
-    format:
-    {
-        indicators: {
-            ...
-        },
-        queries: {
-            query_name_1: query_data_1
-        }
-    }
-    """
-    # todo: just get the data directly from here
     records = []
     price_close_records = data["queries"]["price_close"]
     for d in price_close_records:
@@ -35,18 +23,5 @@ def calculate(data, params):
             "value": d["price_close"]
         })
     return records
-
-
-    records = []
-    query_data = data["queries"]["price_close"]  # note: name of query is price_close
-    # note: reversed because query returns in desc order
-    for d in reversed(query_data["aggregations"]["resample"]["buckets"]):
-        _source = d["close"]["hits"]["hits"][0]["_source"]
-        records.append({
-            "timestamp": _source["timestamp_close"],
-            "value": _source["price_close"],
-        })
-    return records
-
 
 
